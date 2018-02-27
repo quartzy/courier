e2df4ba81b44f2c88575472e9f6f24e3c96c02d1
Chris Muthig <camuthig@gmail.com>
2018-02-26 17:23:00 -0800

    Add a script and try something new

---
+++ deploy_docs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  git config user.name ${GH_USER_NAME}
+  git config user.email ${GH_USER_EMAIL}
+  git remote add gh-token "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git";
+  git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
+  pip install -r requirements.txt;
+  mkdocs gh-deploy -v --clean --remote-name gh-token;
+fi
