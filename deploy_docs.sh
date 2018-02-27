57b822f4dc0b8d9748f8783987062c1544c1f2d9
Chris Muthig <camuthig@gmail.com>
2018-02-26 17:45:38 -0800

    Only deploy to gh-pages once

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
-if [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "$TRAVIS_PHP_VERSION" = "7.1" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
   git config user.name ${GH_USER_NAME}
   git config user.email ${GH_USER_EMAIL}
   git remote add gh-token "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
